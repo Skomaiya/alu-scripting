19411f52229f930d21c0a27c852c60af9034e004
Komaiya Samuel <s.komaiya@alustudent.com>
2023-11-07 03:17:10 -0800

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,21 +5,41 @@
 import json
 import requests
 
-
 """
     Define HTTP headers for the API requests
 """
+
+
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'  # Fixed URL
+    
+    '''
+    Construct the Reddit API URL for the specified subreddit
+    '''
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    '''
+    Send an HTTP GET request to the API with a custom user-agent header
+    '''
     response = requests.get(url, headers={'User-agent': 'I am learning Apis'})
 
     if response.status_code == 200:
+        '''
+        If the response status code is 200 (OK), the request was successful
+        '''
+        '''
+        Parse the response JSON to extract subreddit title and subscribers count
+        '''
         content = response.json()
         result = {"title": content['data']['title'],
                   "subscribers": content['data']['subscribers']}
-        return result
+        print(result)
     elif response.status_code == 404:
+        '''
+        If the response status code is 404, the subreddit was not found
+        '''
         return 0
     else:
-        print(f"Request failed with status code: {response.status_code}")
-        return None
+        '''
+        Handle other response status codes with an exception or specific value
+        '''
+        raise Exception(f"Request failed with status code: {response.status_code}")
